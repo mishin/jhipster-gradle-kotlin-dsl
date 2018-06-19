e8ea2978b04da07b54ed9cf23a20bbe27aac8504
unknown <you@example.com>
2018-06-19 23:50:52 +0300

    Initial application generated by JHipster-5.0.0-beta.2

---
+++ src/main/java/tech/jhipster/config/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package tech.jhipster.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Gradlekotlindsl.
+ * <p>
+ * Properties are configured in the application.yml file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+
+}
